83f901001ef62949ac6725e0937b7057afac6a37
edwinfadul <edwinfadul@gmail.com>
2023-08-31 14:28:37 -0500

    feat: ajustes en backend url

--- config/server.js
+++ config/server.js
@@ -1,8 +1,10 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 1337),
+  url: env("BACKEND_URL", "http://localhost:1337"),
+  proxy: env('PROXY', true),
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: env.array("APP_KEYS"),
   },
   webhooks: {
     populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
